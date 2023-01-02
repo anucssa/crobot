8d846f7df4c589e2c6fff567d69df3b5aa682ba0
Ashley Lamont <ashley@ashl.dev>
2023-01-02 13:37:52 +1100

    fix(bot): Fix race condition

--- src/discordClient.ts
+++ src/discordClient.ts
@@ -2,22 +2,23 @@ import { Client, GatewayIntentBits } from 'discord.js'
 import { config } from 'dotenv'
 
 export async function initDiscord (): Promise<Client<true>> {
-  config()
+  return await new Promise((resolve, reject) => {
+    config()
 
-  const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+    const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-  client.on('ready', () => {
-    client.user?.setPresence({
-      activities: [{
-        name: 'the door sensor boot',
-        type: 3
-      }],
-      status: 'idle'
+    client.on('ready', () => {
+      client.user?.setPresence({
+        activities: [{
+          name: 'the door sensor boot',
+          type: 3
+        }],
+        status: 'idle'
+      })
+      console.log(`Logged into discord as ${client.user?.tag ?? 'USER IS NULL'}!`)
+      resolve(client)
     })
-    console.log(`Logged into discord as ${client.user?.tag ?? 'USER IS NULL'}!`)
+
+    client.login(process.env.DISCORD_TOKEN).catch(reject)
   })
-
-  await client.login(process.env.DISCORD_TOKEN)
-
-  return client
 }
