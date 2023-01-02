edacb7b8bfbb474676f2bdf326d3f3667aa15d0c
Ashley Lamont <ashley@ashl.dev>
2023-01-02 12:28:01 +1100

    fix(bot): Fix typo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { DoorServer } from './doorStatus'
 
 async function main (): Promise<void> {
   const client = await initDiscord()
-  const doorServer = new DoorServer(client)
+  const doorServer = new DoorServer()
   doorServer.startServer(client)
 }
 
